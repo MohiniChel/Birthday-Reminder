a9a100cbd20fa8d786345af0a6804b279a2466bb
mohini <babychel30@gmail.com>
2021-04-28 20:49:57 +0530

    hi

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const persons = [
+    {
+        id: 1,
+        name: 'Mohini Chel',
+        age: 24,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      },
+      {
+        id: 2,
+        name: 'Manu Singh',
+        age: 24,
+        image:
+          ' https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      },
+      {
+        id: 3,
+        name: 'Vijay Kumar',
+        age: 23,
+        image:
+          ' https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      },
+      {
+        id: 4,
+        name: 'Satish Kumar',
+        age: 25,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      },
+      {
+        id: 5,
+        name: 'Lola Gardner',
+        age: 29,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      },
+    ];
+    
+    export default persons;
